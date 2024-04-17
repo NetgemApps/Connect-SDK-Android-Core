70dcba7ff5d12cba9c7cb66c526793ac6c0755d2
Olivier Jouvenaux <olivier.jouvenaux@netgem.com>
2024-04-17 09:29:07 +0200

    Remove for AirPlayService tests: unknown digestAuthentication & getAuthenticate methods

--- test/src/com/connectsdk/service/AirPlayServiceTest.java
+++ test/src/com/connectsdk/service/AirPlayServiceTest.java
@@ -96,6 +96,7 @@ public class AirPlayServiceTest {
         });
     }
 
+    /*
     @Test
     public void testDigestAuthentication() {
         Assert.assertEquals(null, service.digestAuthentication(null));
@@ -103,7 +104,9 @@ public class AirPlayServiceTest {
         Assert.assertEquals("202cb962ac59075b964b07152d234b70", service.digestAuthentication("123"));
         Assert.assertEquals("7b613f0aafa3e72b11d5e08c8c51f03f", service.digestAuthentication("526b828b08a7b3e36498d2ecec4b5e49"));
     }
+    */
 
+    /*
     @Test
     public void testGetAuthenticate() {
         // Assume that a password is AirPlay
@@ -111,6 +114,7 @@ public class AirPlayServiceTest {
         Assert.assertEquals("Digest username=\"AirPlay\", realm=\"AirPlay\", nonce=\"MTMzMTMwODI0MCDEJP5Jo7HFo81rbAcKNKw2\", uri=\"/play\", response=\"85c25341d6e62d402f6600340fc44ce0\"",
                 service.getAuthenticate("Digest", "/play", "Digest realm=\"AirPlay\", nonce=\"MTMzMTMwODI0MCDEJP5Jo7HFo81rbAcKNKw2\""));
     }
+    */
 
     @Test
     public void testInitialPairingType() {
